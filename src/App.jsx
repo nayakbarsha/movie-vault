733a9c7385f218c729ad2dce9b6e11c7b22d4dc2
BARSHA NAYAK <barsha.gml@gmail.com>
2025-07-08 15:36:16 +0530

    added css for all app components, favorite page and routes for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import './App.css';
+import './css/App.css';
+import Favorites from './pages/Favorites.jsx';
 import Home from './pages/Home.jsx';
-
+import {Routes, Route} from "react-router-dom";
+import NavBar from './components/NavBar.jsx';
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <div>
+      <NavBar/>
+      <main className='main-content'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites/>} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
-export default App
+export default App;
---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,11 @@
+import "../css/Favorites.css";
+
+function Favorites(){
+    return (
+        <div>
+            <h2>No favorite movies yet!</h2>
+            <p>Start adding your favorite movies and they will appear here.</p>
+        </div>
+    )
+}
+export default Favorites;
